7fc3fe09d3eeaf21de713f3021023aca0744d707
Kenta Shibuta <kenta@Kenta-no-iMac.local>
2020-08-29 12:33:58 +0900

    update

--- create_image.py
+++ create_image.py
@@ -10,6 +10,7 @@ import pandas as pd
 import time
 from numba import jit
 import functools
+import matplotlib.cm as cm
 
 def create_full_path(file_name, inputDir):
     return inputDir + file_name
@@ -22,8 +23,9 @@ def sort(A):
     B.sort(key = lambda x: float(x) if isd(x) else x)
     return B
 
-def create_f(f_name, zoom_para, width_par_vec, output_val_num, flag_vector_plot, count):
-    plt.cla()  
+def create_f(f_name, zoom_para, width_par_vec, output_val_num, flag_vector_plot, count, flag_domain):
+    plt.cla() 
+    plt.clf() 
 
     # ファイル読み込み
     pd_para_data = pd.read_csv(f_name, nrows=1)
@@ -39,10 +41,30 @@ def create_f(f_name, zoom_para, width_par_vec, output_val_num, flag_vector_plot,
     nc1 = nc+1
     interface_start = (nc1 * nc1) + 4 * nc * (nl1-1)
 
+    if flag_vector_plot == 2:
+        if flag_domain ==  2:
+            s_max = np.max(data[:, int(output_val_num)])
+            s_min = np.min(data[:, int(output_val_num)])
+            sc = plt.scatter(data[:, 1], data[:, 2], vmin=s_min, vmax=s_max, c=data[:, int(output_val_num)], cmap=cm.jet, s=4)
+
+        # 液滴の中だけの分布が見たい
+        if flag_domain ==  1:
+            s_max = np.max(data[:int(interface_start+4*nc-1), int(output_val_num)])
+            s_min = np.min(data[:int(interface_start+4*nc-1), int(output_val_num)])
+            sc = plt.scatter(data[:int(interface_start+4*nc-1), 1], data[:int(interface_start+4*nc-1), 2], vmin=s_min, vmax=s_max, c=data[:int(interface_start+4*nc-1), int(output_val_num)], cmap=cm.jet, s=4)
+        
+        plt.colorbar(sc)
+
+        # 界面描画
+        for i in range(4*nc):
+            if i == 4*nc - 1:
+                plt.plot([data[int(i+interface_start), 1], data[int(interface_start), 1]], [data[int(i+interface_start), 2], data[int(interface_start), 2]],'k-', lw=1.0, label = "Interface")
+            else:
+                plt.plot([data[int(i+interface_start), 1], data[int(i+1+interface_start), 1]], [data[int(i+interface_start), 2], data[int(i+1+interface_start), 2]],'k-', lw=1.0)
 
     # 3: velocity
     # 5: B
-    # 8: al
+    # 10: al
 
     if flag_vector_plot == 1:
 
@@ -61,12 +83,12 @@ def create_f(f_name, zoom_para, width_par_vec, output_val_num, flag_vector_plot,
 
         plt.quiverkey(Q, 0.85, 0.05, base, '%f'%(base), labelpos='E', coordinates='figure')
 
-    # 界面描画
-    for i in range(4*nc):
-        if i == 4*nc - 1:
-            plt.plot([data[int(i+interface_start), 1], data[int(interface_start), 1]], [data[int(i+interface_start), 2], data[int(interface_start), 2]],'m-.', lw=1.0, label = "Interface")
-        else:
-            plt.plot([data[int(i+interface_start), 1], data[int(i+1+interface_start), 1]], [data[int(i+interface_start), 2], data[int(i+1+interface_start), 2]],'m-.', lw=1.0)
+        # 界面描画
+        for i in range(4*nc):
+            if i == 4*nc - 1:
+                plt.plot([data[int(i+interface_start), 1], data[int(interface_start), 1]], [data[int(i+interface_start), 2], data[int(interface_start), 2]],'m-.', lw=1.0, label = "Interface")
+            else:
+                plt.plot([data[int(i+interface_start), 1], data[int(i+1+interface_start), 1]], [data[int(i+interface_start), 2], data[int(i+1+interface_start), 2]],'m-.', lw=1.0)
 
     plt.xlabel('$\it{x}$'+' [m]')
     plt.ylabel('$\it{y}$'+' [m]')
@@ -85,6 +107,7 @@ def main():
     flag_vector_plot = 0
     output_val_num = 3
     skip_num = 1
+    flag_domain = 2
 
     print("input data >>>")
     inputDir = input().strip()+"/"
@@ -95,15 +118,21 @@ def main():
     print("zoom para (default is 1)>>>")
     zoom_para = input()
 
-    print("vector plot on(1) / off(0) (default is 0)>>>")
+    print("scalar plot(2) / vector plot on(1) / off(0) (default is 0)>>>")
     flag_vector_plot = int(input())
 
     if flag_vector_plot == 1:
-        print("kind of vectors. 3:v, 5:B, 8:al (default is 3)>>>")
+        print("kind of vectors. 3:v, 5:B, 10:al (default is 3)>>>")
         output_val_num = int(input())
 
         print("width of plot / the largest vector (default is 20)>>>")
         width_par_vec = input()
+    
+    if flag_vector_plot == 2:
+        print("kind of scalar. 7:Ez, 8: jz, 9:Az >>>")
+        output_val_num = int(input())
+        print("only liquid domain(1) / all aomain(2) >>>")
+        flag_domain = int(input())
 
     list1 = os.listdir(inputDir)
     list2 = sort(list1)
@@ -120,7 +149,7 @@ def main():
 
     for i in range(length):
         if int(i % skip_num) == 0: 
-            create_f(list4[i], zoom_para, width_par_vec, output_val_num, flag_vector_plot, count)
+            create_f(list4[i], zoom_para, width_par_vec, output_val_num, flag_vector_plot, count, flag_domain)
             count += 1
 
 main()
